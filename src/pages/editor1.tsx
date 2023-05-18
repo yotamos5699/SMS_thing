7e59a83a923b3f8a44b152857e104e2dfa55730b
yotamos5699 <m.lhafakot@gmail.com>
2023-05-18 18:37:53 +0300

    asd

--- src/pages/editor1.tsx
+++ src/pages/editor1.tsx
@@ -1,5 +1,13 @@
-import dynamic from "next/dynamic";
-const Editor = dynamic(() =>
-  import("../editor1/editor/page").then((res) => res.Editor)
-);
-export default Editor;
+// import dynamic from "next/dynamic";
+// const Editor = dynamic(() =>
+//   import("../editor1/editor/page").then((res) => res.Editor)
+// );
+// export default Editor;
+
+import React from "react";
+
+function editor1() {
+  return <div>editor1 not working yet... </div>;
+}
+
+export default editor1;
